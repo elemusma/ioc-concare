29aa702ebf383c10910fe49ff689a08785d98a25
Efrain Lemus <efraintlemusm@gmail.com>
2023-02-16 11:01:29 -0700

    remove tawk chat and move footer code from individual files to main footer

--- epoxy-concrete-densifier-concrete-penetrant-floors.php
+++ epoxy-concrete-densifier-concrete-penetrant-floors.php
@@ -69,35 +69,5 @@
         
         <?php require_once("footer.php"); ?>
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-        <script src="js/dropit.js"></script>
-        <script src="js/dropdown.js"></script>
-
-       <script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-			ga('create', 'UA-15819292-1', 'auto');
-			ga('send', 'pageview');
-
-		</script>
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5938695f4374a471e7c51efa/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
-
 </body>
 </html>
